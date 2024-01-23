cf421dc686556f0abbde7ea09e02f1e20c13fe50
Cyril K.B. <53737317+Cykyrios@users.noreply.github.com>
2024-01-23 01:16:54 +0100

    Update demo with updated GodotInSim

--- demo.gd
+++ demo.gd
@@ -4,6 +4,10 @@ extends MarginContainer
 @export var insim_address := "127.0.0.1"
 @export var insim_port := 29_999
 
+var insim := InSim.new()
+var outsim := OutSim.new()
+var outgauge := OutGauge.new()
+
 var car_lights_timer := Timer.new()
 var car_switches_timer := Timer.new()
 
@@ -20,23 +24,26 @@ func _ready() -> void:
 	_discard = car_lights_button.pressed.connect(_on_car_lights_button_pressed)
 	_discard = car_switches_button.pressed.connect(_on_car_switches_button_pressed)
 
-	GISOutGauge.initialize()
-	GISOutSim.initialize(0x1ff)
-	_discard = GISOutGauge.packet_received.connect(update_outgauge)
-	_discard = GISOutSim.packet_received.connect(update_outsim)
+	add_child(outgauge)
+	outgauge.initialize()
+	add_child(outsim)
+	outsim.initialize(0x1ff)
+	_discard = outgauge.packet_received.connect(update_outgauge)
+	_discard = outsim.packet_received.connect(update_outsim)
 	initialize_timers()
 
-	_discard = GISInSim.packet_received.connect(func(packet: InSimPacket) -> void:
+	add_child(insim)
+	_discard = insim.packet_received.connect(func(packet: InSimPacket) -> void:
 		print("Received packet ", packet)
 		print(packet.get_dictionary())
 	)
 
 
 func _exit_tree() -> void:
-	if GISInSim.insim_connected:
-		GISInSim.close()
-	GISOutGauge.close()
-	GISOutSim.close()
+	if insim.insim_connected:
+		insim.close()
+	outgauge.close()
+	outsim.close()
 
 
 func initialize_timers() -> void:
@@ -47,14 +54,14 @@ func initialize_timers() -> void:
 
 
 func _on_insim_button_pressed() -> void:
-	if GISInSim.socket.is_socket_connected():
-		GISInSim.close()
+	if insim.socket.is_socket_connected():
+		insim.close()
 		insim_button.text = "Initialize InSim"
 	else:
-		GISInSim.address = insim_address
-		GISInSim.insim_port = insim_port
+		insim.address = insim_address
+		insim.insim_port = insim_port
 		var initialization_data := InSimInitializationData.new()
-		GISInSim.initialize(initialization_data)
+		insim.initialize(initialization_data)
 		insim_button.text = "Close InSim"
 
 
@@ -62,10 +69,10 @@ func _on_car_lights_button_pressed() -> void:
 	if car_lights_timer.is_stopped():
 		car_lights_timer.start(0.1)
 		car_lights_button.text = "Stop Car Lights"
-		GISInSim.send_local_car_lights(CarLights.new())
+		insim.send_local_car_lights(CarLights.new())
 	else:
 		car_lights_timer.stop()
-		GISInSim.send_local_car_lights(CarLights.new(CarLights.ALL_OFF))
+		insim.send_local_car_lights(CarLights.new(CarLights.ALL_OFF))
 		car_lights_button.text = "Send Random Car Lights"
 
 
@@ -75,7 +82,7 @@ func _on_car_switches_button_pressed() -> void:
 		car_switches_button.text = "Stop Car Switches"
 	else:
 		car_switches_timer.stop()
-		GISInSim.send_local_car_switches(CarSwitches.new(CarSwitches.ALL_OFF))
+		insim.send_local_car_switches(CarSwitches.new(CarSwitches.ALL_OFF))
 		car_switches_button.text = "Send Random Car Switches"
 
 
@@ -91,7 +98,7 @@ func send_random_lights() -> void:
 	lcl.fog_rear = randi() % 2
 	lcl.fog_front = randi() % 2
 	lcl.extra = randi() % 2
-	GISInSim.send_local_car_lights(lcl)
+	insim.send_local_car_lights(lcl)
 
 
 func send_random_switches() -> void:
@@ -106,7 +113,7 @@ func send_random_switches() -> void:
 	lcs.headlights = randi() % 2
 	lcs.horn = randi() % 6
 	lcs.siren = randi() % 3
-	GISInSim.send_local_car_switches(lcs)
+	insim.send_local_car_switches(lcs)
 
 
 func update_outgauge(outgauge_packet: OutGaugePacket) -> void:
@@ -137,60 +144,48 @@ func update_outgauge(outgauge_packet: OutGaugePacket) -> void:
 
 func update_outsim(outsim_packet: OutSimPacket) -> void:
 	outsim_label.text = "OutSim:"
-	if outsim_packet is OutSimPacketSimple:
-		var outsim_pack := (outsim_packet as OutSimPacketSimple).outsim_pack
-		outsim_label.text += "\n%s: %s" % ["Time", outsim_pack.time]
-		outsim_label.text += "\n%s: %s" % ["Angular Velocity", outsim_pack.ang_vel]
-		outsim_label.text += "\n%s: %s" % ["Heading", outsim_pack.heading]
-		outsim_label.text += "\n%s: %s" % ["Pitch", outsim_pack.pitch]
-		outsim_label.text += "\n%s: %s" % ["Roll", outsim_pack.roll]
-		outsim_label.text += "\n%s: %s" % ["Acceleration", outsim_pack.accel]
-		outsim_label.text += "\n%s: %s" % ["Velocity", outsim_pack.vel]
-		outsim_label.text += "\n%s: %s" % ["Position", outsim_pack.pos]
-		outsim_label.text += "\n%s: %s" % ["ID", outsim_pack.id]
-	elif outsim_packet is OutSimPacketDetailed:
-		var pack := (outsim_packet as OutSimPacketDetailed).outsim_pack
-		var os_options := (outsim_packet as OutSimPacketDetailed).outsim_pack.os_options
-		if os_options & OutSim.OutSimOpts.OSO_HEADER:
-			outsim_label.text += "\n%s%s%s%s" % [pack.header_l, pack.header_f, pack.header_s, pack.header_t]
-		if os_options & OutSim.OutSimOpts.OSO_ID:
-			outsim_label.text += "\n%s: %d" % ["ID", pack.id]
-		if os_options & OutSim.OutSimOpts.OSO_TIME:
-			outsim_label.text += "\n%s: %d" % ["Time", pack.time]
-		if os_options & OutSim.OutSimOpts.OSO_MAIN:
-			var outsim_main := pack.os_main
-			outsim_label.text += "\n%s: %.2v" % ["AngVel", outsim_main.ang_vel]
-			outsim_label.text += "\n%s: %.2f, %s: %.2f, %s: %.2f" % \
-					["Heading", outsim_main.heading, "Pitch", outsim_main.pitch, "Roll", outsim_main.roll]
-			outsim_label.text += "\n%s: %.2v" % ["Accel", outsim_main.accel]
-			outsim_label.text += "\n%s: %.2v" % ["Vel", outsim_main.vel]
-			outsim_label.text += "\n%s: %s" % ["Pos", outsim_main.pos]
-		if os_options & OutSim.OutSimOpts.OSO_INPUTS:
-			var outsim_inputs := pack.os_inputs
-			outsim_label.text += "\n%s: %.2f, %s: %.2f, %s: %.2f, %s: %.2f, %s: %.2f" % \
-					["Throttle", outsim_inputs.throttle, "Brake", outsim_inputs.brake,
-					"InputSteer", outsim_inputs.input_steer, "Clutch", outsim_inputs.clutch,
-					"Handbrake", outsim_inputs.handbrake]
-		if os_options & OutSim.OutSimOpts.OSO_DRIVE:
-			outsim_label.text += "\n%s: %d" % ["Gear", pack.gear]
-			outsim_label.text += "\n%s: %.2f" % ["EngineAngVel", pack.engine_ang_vel]
-			outsim_label.text += "\n%s: %.2f" % ["MaxTorqueAtVel", pack.max_torque_at_vel]
-		if os_options & OutSim.OutSimOpts.OSO_DISTANCE:
-			outsim_label.text += "\n%s: %.2f" % ["CurrentLapDistance", pack.current_lap_distance]
-			outsim_label.text += "\n%s: %.2f" % ["IndexedDistance", pack.indexed_distance]
-		if os_options & OutSim.OutSimOpts.OSO_WHEELS:
-			var outsim_wheels := pack.os_wheels
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["SuspDeflect", outsim_wheels[0].susp_deflect, outsim_wheels[1].susp_deflect, outsim_wheels[2].susp_deflect, outsim_wheels[3].susp_deflect]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["Steer", outsim_wheels[0].steer, outsim_wheels[1].steer, outsim_wheels[2].steer, outsim_wheels[3].steer]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["XForce", outsim_wheels[0].x_force, outsim_wheels[1].x_force, outsim_wheels[2].x_force, outsim_wheels[3].x_force]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["YForce", outsim_wheels[0].y_force, outsim_wheels[1].y_force, outsim_wheels[2].y_force, outsim_wheels[3].y_force]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["VerticalLoad", outsim_wheels[0].vertical_load, outsim_wheels[1].vertical_load, outsim_wheels[2].vertical_load, outsim_wheels[3].vertical_load]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["AngVel", outsim_wheels[0].ang_vel, outsim_wheels[1].ang_vel, outsim_wheels[2].ang_vel, outsim_wheels[3].ang_vel]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["LeanRelToRoad", outsim_wheels[0].lean_rel_to_road, outsim_wheels[1].lean_rel_to_road, outsim_wheels[2].lean_rel_to_road, outsim_wheels[3].lean_rel_to_road]
-			outsim_label.text += "\n%s: %d/%d/%d/%d" % ["AirTemp", outsim_wheels[0].air_temp, outsim_wheels[1].air_temp, outsim_wheels[2].air_temp, outsim_wheels[3].air_temp]
-			outsim_label.text += "\n%s: %d/%d/%d/%d" % ["SlipFraction", outsim_wheels[0].slip_fraction, outsim_wheels[1].slip_fraction, outsim_wheels[2].slip_fraction, outsim_wheels[3].slip_fraction]
-			outsim_label.text += "\n%s: %d/%d/%d/%d" % ["Touching", outsim_wheels[0].touching, outsim_wheels[1].touching, outsim_wheels[2].touching, outsim_wheels[3].touching]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["SlipRatio", outsim_wheels[0].slip_ratio, outsim_wheels[1].slip_ratio, outsim_wheels[2].slip_ratio, outsim_wheels[3].slip_ratio]
-			outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["TanSlipAngle", outsim_wheels[0].tan_slip_angle, outsim_wheels[1].tan_slip_angle, outsim_wheels[2].tan_slip_angle, outsim_wheels[3].tan_slip_angle]
-		if os_options & OutSim.OutSimOpts.OSO_EXTRA_1:
-			outsim_label.text += "\n%s: %.2f" % ["SteerTorque", pack.steer_torque]
+	var pack := outsim_packet.outsim_pack
+	var os_options := pack.outsim_options
+	if os_options & OutSim.OutSimOpts.OSO_HEADER:
+		outsim_label.text += "\n%s%s%s%s" % [pack.header_l, pack.header_f, pack.header_s, pack.header_t]
+	if os_options & OutSim.OutSimOpts.OSO_ID:
+		outsim_label.text += "\n%s: %d" % ["ID", pack.id]
+	if os_options & OutSim.OutSimOpts.OSO_TIME:
+		outsim_label.text += "\n%s: %d" % ["Time", pack.time]
+	if os_options & OutSim.OutSimOpts.OSO_MAIN:
+		var outsim_main := pack.os_main
+		outsim_label.text += "\n%s: %.2v" % ["AngVel", outsim_main.ang_vel]
+		outsim_label.text += "\n%s: %.2f, %s: %.2f, %s: %.2f" % \
+				["Heading", outsim_main.heading, "Pitch", outsim_main.pitch, "Roll", outsim_main.roll]
+		outsim_label.text += "\n%s: %.2v" % ["Accel", outsim_main.accel]
+		outsim_label.text += "\n%s: %.2v" % ["Vel", outsim_main.vel]
+		outsim_label.text += "\n%s: %s" % ["Pos", outsim_main.pos]
+	if os_options & OutSim.OutSimOpts.OSO_INPUTS:
+		var outsim_inputs := pack.os_inputs
+		outsim_label.text += "\n%s: %.2f, %s: %.2f, %s: %.2f, %s: %.2f, %s: %.2f" % \
+				["Throttle", outsim_inputs.throttle, "Brake", outsim_inputs.brake,
+				"InputSteer", outsim_inputs.input_steer, "Clutch", outsim_inputs.clutch,
+				"Handbrake", outsim_inputs.handbrake]
+	if os_options & OutSim.OutSimOpts.OSO_DRIVE:
+		outsim_label.text += "\n%s: %d" % ["Gear", pack.gear]
+		outsim_label.text += "\n%s: %.2f" % ["EngineAngVel", pack.engine_ang_vel]
+		outsim_label.text += "\n%s: %.2f" % ["MaxTorqueAtVel", pack.max_torque_at_vel]
+	if os_options & OutSim.OutSimOpts.OSO_DISTANCE:
+		outsim_label.text += "\n%s: %.2f" % ["CurrentLapDistance", pack.current_lap_distance]
+		outsim_label.text += "\n%s: %.2f" % ["IndexedDistance", pack.indexed_distance]
+	if os_options & OutSim.OutSimOpts.OSO_WHEELS:
+		var outsim_wheels := pack.os_wheels
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["SuspDeflect", outsim_wheels[0].susp_deflect, outsim_wheels[1].susp_deflect, outsim_wheels[2].susp_deflect, outsim_wheels[3].susp_deflect]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["Steer", outsim_wheels[0].steer, outsim_wheels[1].steer, outsim_wheels[2].steer, outsim_wheels[3].steer]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["XForce", outsim_wheels[0].x_force, outsim_wheels[1].x_force, outsim_wheels[2].x_force, outsim_wheels[3].x_force]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["YForce", outsim_wheels[0].y_force, outsim_wheels[1].y_force, outsim_wheels[2].y_force, outsim_wheels[3].y_force]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["VerticalLoad", outsim_wheels[0].vertical_load, outsim_wheels[1].vertical_load, outsim_wheels[2].vertical_load, outsim_wheels[3].vertical_load]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["AngVel", outsim_wheels[0].ang_vel, outsim_wheels[1].ang_vel, outsim_wheels[2].ang_vel, outsim_wheels[3].ang_vel]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["LeanRelToRoad", outsim_wheels[0].lean_rel_to_road, outsim_wheels[1].lean_rel_to_road, outsim_wheels[2].lean_rel_to_road, outsim_wheels[3].lean_rel_to_road]
+		outsim_label.text += "\n%s: %d/%d/%d/%d" % ["AirTemp", outsim_wheels[0].air_temp, outsim_wheels[1].air_temp, outsim_wheels[2].air_temp, outsim_wheels[3].air_temp]
+		outsim_label.text += "\n%s: %d/%d/%d/%d" % ["SlipFraction", outsim_wheels[0].slip_fraction, outsim_wheels[1].slip_fraction, outsim_wheels[2].slip_fraction, outsim_wheels[3].slip_fraction]
+		outsim_label.text += "\n%s: %d/%d/%d/%d" % ["Touching", outsim_wheels[0].touching, outsim_wheels[1].touching, outsim_wheels[2].touching, outsim_wheels[3].touching]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["SlipRatio", outsim_wheels[0].slip_ratio, outsim_wheels[1].slip_ratio, outsim_wheels[2].slip_ratio, outsim_wheels[3].slip_ratio]
+		outsim_label.text += "\n%s: %.2f/%.2f/%.2f/%.2f" % ["TanSlipAngle", outsim_wheels[0].tan_slip_angle, outsim_wheels[1].tan_slip_angle, outsim_wheels[2].tan_slip_angle, outsim_wheels[3].tan_slip_angle]
+	if os_options & OutSim.OutSimOpts.OSO_EXTRA_1:
+		outsim_label.text += "\n%s: %.2f" % ["SteerTorque", pack.steer_torque]
